ef1f52aa08615a17258992b5db12b260946824f3
Dimitris Papastavridis <dimitris_papast@hotmail.com>
2025-07-07 20:24:21 +0300

    myfirstcommit

---
+++ nba-trades.py
@@ -0,0 +1,117 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+from datetime import datetime
+import os
+
+url = "https://stats.nba.com/js/data/playermovement/NBA_Player_Movement.json"
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "Referer": "https://www.nba.com/",
+    "Origin": "https://www.nba.com"
+}
+
+response = requests.get(url, headers=headers)
+
+data = response.json()
+
+df = pd.DataFrame(data['NBA_Player_Movement']['rows'])
+
+df['TRANSACTION_DATE'] = pd.to_datetime(df['TRANSACTION_DATE'])
+
+df['year'] = df['TRANSACTION_DATE'].dt.year
+df['month'] = df['TRANSACTION_DATE'].dt.month
+
+df['PLAYER_SLUG'] = df['PLAYER_SLUG'].apply(lambda x : ' '.join([component.capitalize() for component in x.split('-')]))
+
+df['TEAM_SLUG'] = df['TEAM_SLUG'].apply(lambda x : x.capitalize())
+
+df['TEAM_RECEIVING_PLAYER'] = df[ df['Transaction_Type'] == 'Trade' ]['TRANSACTION_DESCRIPTION'].apply(lambda x : x.split('received')[0].strip())
+
+df['TEAM_RECEIVING_PLAYER'] = df['TEAM_RECEIVING_PLAYER'].fillna(df['TEAM_SLUG'])
+
+df['TEAM_SENDING_PLAYER'] = df[ df['Transaction_Type'] == 'Trade' ]['TRANSACTION_DESCRIPTION'].apply(lambda x : x.split('from')[1].replace('.','').strip())
+
+df['TEAM_SENDING_PLAYER'] = df['TEAM_SENDING_PLAYER'].fillna('-')
+
+team_name_map = {
+    'Hawks': 'Atlanta Hawks',
+    'Celtics': 'Boston Celtics',
+    'Nets': 'Brooklyn Nets',
+    'Hornets': 'Charlotte Hornets',
+    'Bulls': 'Chicago Bulls',
+    'Cavaliers': 'Cleveland Cavaliers',
+    'Mavericks': 'Dallas Mavericks',
+    'Nuggets': 'Denver Nuggets',
+    'Pistons': 'Detroit Pistons',
+    'Warriors': 'Golden State Warriors',
+    'Rockets': 'Houston Rockets',
+    'Pacers': 'Indiana Pacers',
+    'Clippers': 'LA Clippers',
+    'Lakers': 'Los Angeles Lakers',
+    'Grizzlies': 'Memphis Grizzlies',
+    'Heat': 'Miami Heat',
+    'Bucks': 'Milwaukee Bucks',
+    'Timberwolves': 'Minnesota Timberwolves',
+    'Pelicans': 'New Orleans Pelicans',
+    'Knicks': 'New York Knicks',
+    'Thunder': 'Oklahoma City Thunder',
+    'Magic': 'Orlando Magic',
+    'Sixers': 'Philadelphia 76ers',
+    'Suns': 'Phoenix Suns',
+    'Blazers': 'Portland Trail Blazers',
+    'Kings': 'Sacramento Kings',
+    'Spurs': 'San Antonio Spurs',
+    'Raptors': 'Toronto Raptors',
+    'Jazz': 'Utah Jazz',
+    'Wizards': 'Washington Wizards'
+}
+
+df['TEAM_RECEIVING_PLAYER'] = df['TEAM_RECEIVING_PLAYER'].replace(team_name_map)
+
+df = df[['Transaction_Type','GroupSort','PLAYER_SLUG','TEAM_RECEIVING_PLAYER','TEAM_SENDING_PLAYER','TRANSACTION_DATE','year','month']]
+
+df.columns = [col.lower() for col in df.columns]
+
+if not os.path.isfile('/app/csv/nba-trades.csv'):
+   df.to_csv('/app/csv/nba-trades.csv', index= False)
+else:
+   df.to_csv('/app/csv/nba-trades.csv', mode='a', index=False, header=False)
+
+
+engine = create_engine(
+'postgresql+psycopg2:'
+'//postgres:'          # username for postgres
+'docker'              # password for postgres
+'@postgresdb:5432/'        # postgres server name and the exposed port
+'postgres')
+
+con = engine.connect()
+
+sql = """
+    create table if not exists nba_trades (
+    transaction_type VARCHAR(50),
+    groupsort VARCHAR(50),
+    player_slug VARCHAR(50),
+    team_receiving_player VARCHAR(50),
+    team_sending_player VARCHAR(50),
+    transaction_date DATE,
+    year INT,
+    month INT
+);
+"""
+
+# execute the 'sql' query
+with engine.connect().execution_options(autocommit=True) as conn:
+    query = conn.execute(text(sql))
+
+with engine.connect().execution_options(autocommit=True) as conn:
+    df.to_sql('nba_trades', con=conn, if_exists='append', index= False)
+
+#optional
+print(pd.read_sql_query("""
+select * from nba_trades
+""", con))
